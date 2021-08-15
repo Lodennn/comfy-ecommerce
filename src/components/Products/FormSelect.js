2f5388af538befeb0b4345d48349bb8cc4903711
Lodenn <khalednasser788@gmail.com>
2021-08-15 04:35:39 +0200

    add filter component and prepare functionality

---
+++ src/components/Products/FormSelect.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { filterActions } from "../../store/filter-slice";
+import classes from "./FormSelect.module.css";
+
+const FormSelect = (props) => {
+  const dispatch = useDispatch();
+  const onSelectChange = (e) => {
+    e.preventDefault();
+    console.log(e.target.name);
+    dispatch(filterActions.sortBy(e.target.value));
+  };
+  return (
+    <form onChange={onSelectChange}>
+      <label htmlFor="sort">Sort By</label>
+      <select name="sort" id="sort" className={classes["sort-input"]}>
+        <option value="price-lowest">price (lowest)</option>
+        <option value="price-highest">price (highest)</option>
+        <option value="name-a">name (a-z)</option>
+        <option value="name-z">name (z-a)</option>
+      </select>
+    </form>
+  );
+};
+
+export default FormSelect;
